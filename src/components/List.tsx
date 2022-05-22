f97c9bf660bc6756888c5ea19d4f4c02c98945c8
sanjana1327705 <83221615+sanjana1327705@users.noreply.github.com>
2022-05-22 19:52:45 +0530

    Add files via upload

---
+++ src/components/List.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+//import  { useEffect } from "react";
+import "./List.css";
+import Item from './Item';
+import { StoryType } from "../types";
+import { useState } from "react";
+
+type ListProps = {
+  listOfItems: Array<StoryType>;
+};
+
+const SORT_COLUMNS = {
+  
+  title: (a: StoryType, b: StoryType) => a?.title?.localeCompare(b?.title),
+  url: (a: StoryType, b: StoryType) => a?.url?.localeCompare(b?.url),
+  author: (a: StoryType, b: StoryType) => a?.author?.localeCompare(b?.author),
+};
+
+const List = ({ listOfItems }: ListProps) => {
+  const [sortedList, setSortedList] = useState(listOfItems);
+
+  function handleSort(column:  "title" | "url" | "author") {
+    const sortedListOfItems = [...sortedList.sort(SORT_COLUMNS[column])];
+    setSortedList(sortedListOfItems);
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+           
+            <th onClick={() => handleSort("title")}>Title</th>
+            <th onClick={() => handleSort("url")}>URL</th>
+            <th onClick={() => handleSort("author")}>Author</th>
+            <th>Comments</th>
+            <th>Page Number</th>
+            <th>Action</th>
+           
+          </tr>
+        </thead>
+        <tbody>
+          {sortedList.map((item) => (
+            <Item key={item.objectID} item={item} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default React.memo(List);
